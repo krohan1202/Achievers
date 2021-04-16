fa3e178654349c3e1d47c5575fc0994b85b63428
Rohan Karmakar <krohan1202@gmail.com>
2021-04-16 09:58:08 +0530

    Update Hall of Fame

--- Achievers/frontend/src/components/mainpages/products/Products.js
+++ Achievers/frontend/src/components/mainpages/products/Products.js
@@ -277,10 +277,24 @@ function Products() {
             <h1 className="ach__hallOfFame--heading">ACHIEVER'S HALL OF FAME</h1>
             <hr className="ach__hallOfFame--underline"></hr>
 
-            <span className="ach__hallOfFame--vidCard">
-                <p className="ach__hallOfFame--vidCardHeading">Just testing itJust testing itJust testing itJust testing itJust testing itJust testing it</p>
-                <iframe className="ach__hallOfFame--vidCardLink" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
-            </span>
+            <div className="ach__hallOfFame--vidCards">
+                <span className="ach__hallOfFame--vidCard">
+                    <p className="ach__hallOfFame--vidCardHeading">CHEM ACADEMY LIVE IS BACK | SUBSCRIBE THE CHANNEL | TARGET 100K | IIT-JAM | CSIRNET | GATE | JEE NEET</p>
+                    <iframe className="ach__hallOfFame--vidCardLink" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+                </span>
+                <span className="ach__hallOfFame--vidCard">
+                    <p className="ach__hallOfFame--vidCardHeading">CHEM ACADEMY LIVE IS BACK | SUBSCRIBE THE CHANNEL | TARGET 100K | IIT-JAM | CSIRNET | GATE | JEE NEET</p>
+                    <iframe className="ach__hallOfFame--vidCardLink" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+                </span>
+                <span className="ach__hallOfFame--vidCard">
+                    <p className="ach__hallOfFame--vidCardHeading">CHEM ACADEMY LIVE IS BACK | SUBSCRIBE THE CHANNEL | TARGET 100K | IIT-JAM | CSIRNET | GATE | JEE NEET</p>
+                    <iframe className="ach__hallOfFame--vidCardLink" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+                </span>
+                <span className="ach__hallOfFame--vidCard">
+                    <p className="ach__hallOfFame--vidCardHeading">CHEM ACADEMY LIVE IS BACK | SUBSCRIBE THE CHANNEL | TARGET 100K | IIT-JAM | CSIRNET | GATE | JEE NEET</p>
+                    <iframe className="ach__hallOfFame--vidCardLink" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+                </span>
+            </div>    
         </div>
         </>
     )
