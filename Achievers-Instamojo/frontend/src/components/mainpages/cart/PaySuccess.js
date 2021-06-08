bb70658d6a2521d5a34c21203f45d7e36119cabd
Rohan Karmakar <krohan1202@gmail.com>
2021-06-08 23:27:04 +0530

    Update1 success pg

--- Achievers-Instamojo/frontend/src/components/mainpages/cart/PaySuccess.js
+++ Achievers-Instamojo/frontend/src/components/mainpages/cart/PaySuccess.js
@@ -3,9 +3,9 @@ import React from "react";
 function PaySuccess() {
     return(
         <div className="ach__success">
-            <h1 className="ach__success--heading">Your Payment was successful</h1>
-            <p>Thanks for buying from us. Hope to see you soon!</p>
-            <a href="/">Go Back</a>
+            <h1 className="ach__success--heading">Your Payment was successful 🎉</h1>
+            <p className="ach__success--thankYou">Thanks for buying from us. Hope to see you soon!</p>
+            <a href="/" className="ach__success--redirect">Back to Home Page</a>
         </div>
     )
 }
