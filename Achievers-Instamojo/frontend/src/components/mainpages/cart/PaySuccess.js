46ab8998ba378d9beb433ac28f9c62f79d59e853
Rohan Karmakar <krohan1202@gmail.com>
2021-06-09 01:23:33 +0530

    Update Logo in successPg

--- Achievers-Instamojo/frontend/src/components/mainpages/cart/PaySuccess.js
+++ Achievers-Instamojo/frontend/src/components/mainpages/cart/PaySuccess.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../../assets/Pics/Home/1-Navbar/Logo.png";
+import Logo from "../../../assets/Pics/Success/successPgLogo.png";
 
 function PaySuccess() {
     return(
